3046247e408dc37e59230312d3b91dca185ab74f
wuyihao <wuyihao@xiao100.com>
2020-01-10 16:41:30 +0800

    fix:

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,6 +1,4 @@
 const path = require('path');
-const webpack = require('webpack');
-const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin');
 const CompressionWebpackPlugin = require('compression-webpack-plugin');
 
 const config = require('./base.config');
@@ -34,23 +32,12 @@ module.exports = {
         ]
     },
     plugins: [
-        // UglifyJs do not support ES6+, you can also use babel-minify for better treeshaking: https://github.com/babel/minify
-        /*new webpack.optimize.minimize({
-            comments: false,
-            compress: {
-                warnings: false,
-                drop_console: true,
-                pure_funcs: ['console.log']
-            },
-            sourceMap: config.productionSourceMap,
-            parallel: true
-        }),*/
-        /*new CompressionWebpackPlugin({
-            asset: '[path].gz[query]',
+        new CompressionWebpackPlugin({
+            filename: '[path].gz[query]',
             algorithm: 'gzip',
             test: new RegExp('\\.(js)$'),
             threshold: 10240,
             minRatio: 0.8
-        })*/
+        })
     ]
 };
